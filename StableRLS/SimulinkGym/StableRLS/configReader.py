f6c3debb65223f4ba3ff9a1b082973dda33fb523
Robert Annuth <robert.annuth@tuhh.de>
2023-01-12 13:56:47 +0100

    adding documentation and todos for configreader

--- StableRLS/SimulinkGym/StableRLS/configReader.py
+++ StableRLS/SimulinkGym/StableRLS/configReader.py
@@ -1,27 +1,38 @@
 # Author Robert Annuth - robert.annuth@tuhh.de
 from configparser import ConfigParser
 
+
 class configReader:
+    """
+    class to read configparser files and get specific sections of the config
+    """
+
     def __init__(self, config):
         parser = ConfigParser()
-        parser.optionxform = str #case sensitive
+        parser.optionxform = str  # case sensitive
         found = parser.read(config)
         if not found:
             raise ValueError('No config file found!')
         self.parser = parser
-    
+
+    # return list of available sections
     def getSections(self):
         return list(dict(self.parser).keys())
 
+    # get one specified section from config
     def get(self, section):
         return smartParse(dict(self.parser.items(section)))
 
+    # get multiple sections from config
+    # TODO this should also work for one config
     def getMulti(self, sections):
         res = {}
         for section in sections:
             res[section] = smartParse(dict(self.parser.items(section)))
         return res
 
+    # return the Ray config section with correct dict
+    # TODO this can be removed
     def getAgent(self):
         cfg = self.get('Ray')
         sections = self.getSections()
@@ -29,11 +40,24 @@ class configReader:
         cfg['env_config'] = self.getMulti(sections)
         return cfg
 
+
 def smartParse(obj):
+    """
+    convert the strings of a (nested) dict to the correct datatype.
+
+    Parameters:
+        obj (dict): dictionary containing strings as values
+
+    Returns:
+        dict: dictionary containing the correct datatype
+    """
+
     if isinstance(obj, dict):
         return {k: smartParse(v) for k, v in obj.items()}
+
     if isinstance(obj, list):
         return [smartParse(elem) for elem in obj]
+
     if isinstance(obj, str):
         if obj == 'None':
             return None
