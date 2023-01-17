512e09934c1eedfb6a9740f35227bf4848f49c39
Robert Annuth <robert.annuth@tuhh.de>
2023-01-17 14:48:32 +0100

    starting to add documentation strings to package files

--- StableRLS/SimulinkGym/StableRLS/createFMU.py
+++ StableRLS/SimulinkGym/StableRLS/createFMU.py
@@ -1,38 +1,44 @@
 # Author Robert Annuth - robert.annuth@tuhh.de
 import matlab.engine
-import sys, os, io
+import os
 
-DEBUG = os.getenv("DEBUG", False) is not None
-LOG = os.getenv("LOG", False) is not None
-
+# specify section containing FMU path
 Section = 'FMU'
+
 
 def createFMU(cfg):
     """
     see https://www.mathworks.com/help/matlab/matlab_external/install-the-matlab-engine-for-python.html
     for the installation of matlab engine
+
+    Parameters:
+        dict: dictionary containing the keys 'fmuPath', 'dt' within the section specified above
     """
-    # start engine
-    eng = matlab.engine.start_matlab('-nosplash -noFigureWindows -r')						
+
+    # start matlab engine
+    eng = matlab.engine.start_matlab('-nosplash -noFigureWindows -r')
     slxDir = os.path.dirname(cfg[Section]['fmuPath'])
     mdl = os.path.splitext(os.path.basename(cfg[Section]['fmuPath']))[0]
-    
-    # based on makeFMU.m script
+
+    # the code is also available as matlab code /Example/matlab/template/makeFMU.m
     eng.eval(f"cd('{slxDir}')", nargout=0)
-    #eng.eval("addpath(pwd)", nargout=0)
-    eng.eval(f"mdl = '{mdl}';" ,nargout=0)
-    #eng.eval("gitCommit",nargout=0)
-    eng.eval(f"Ts = {cfg[Section]['dt']};" ,nargout=0)
-    eng.eval("open_system(mdl)",nargout=0)
-    eng.eval("makeBus",nargout=0)
-    eng.eval("set_param(mdl,'SolverType','Fixed-step')",nargout=0)
-    eng.eval("set_param(mdl,'FixedStep',string(Ts))",nargout=0)
+    eng.eval(f"mdl = '{mdl}';", nargout=0)
+    # dt specifies the step time of the FMU
+    eng.eval(f"Ts = {cfg[Section]['dt']};", nargout=0)
+    eng.eval("open_system(mdl)", nargout=0)
+    eng.eval("makeBus", nargout=0)
+    eng.eval("set_param(mdl,'SolverType','Fixed-step')", nargout=0)
+    eng.eval("set_param(mdl,'FixedStep',string(Ts))", nargout=0)
 
     # Get Measurement Bus
-    eng.eval("set_param([mdl '/Measurement'],'OutDataTypeStr','Inherit: auto');",nargout=0)
-    eng.eval("busInfo = Simulink.Bus.createObject(mdl ,[mdl '/Measurement']);",nargout=0)
-    eng.eval("set_param([mdl '/Measurement'], 'OutDataTypeStr',['Bus: ' busInfo.busName]);",nargout=0)
+    eng.eval(
+        "set_param([mdl '/Measurement'],'OutDataTypeStr','Inherit: auto');", nargout=0)
+    eng.eval(
+        "busInfo = Simulink.Bus.createObject(mdl ,[mdl '/Measurement']);", nargout=0)
+    eng.eval(
+        "set_param([mdl '/Measurement'], 'OutDataTypeStr',['Bus: ' busInfo.busName]);", nargout=0)
 
-    eng.eval("exportToFMU2CS(mdl, 'CreateModelAfterGeneratingFMU', 'off', 'AddIcon', 'off');",nargout=0)
-    eng.eval("Simulink.data.dictionary.closeAll('-discard')",nargout=0)
+    eng.eval(
+        "exportToFMU2CS(mdl, 'CreateModelAfterGeneratingFMU', 'off', 'AddIcon', 'off');", nargout=0)
+    eng.eval("Simulink.data.dictionary.closeAll('-discard')", nargout=0)
     eng.quit()
--- StableRLS/SimulinkGym/StableRLS/fmutools.py
+++ StableRLS/SimulinkGym/StableRLS/fmutools.py
@@ -1,96 +1,97 @@
 # Author Robert Annuth - robert.annuth@tuhh.de
-DEBUG = os.getenv("DEBUG", False) is not None
-LOG = os.getenv("LOG", False) is not None
 
-import shutil
-from fmpy import read_model_description, extract
 from fmpy.fmi2 import FMU2Slave
+from fmpy import read_model_description, extract
+import shutil
+import logging
+logger = logging.getLogger(__name__)
 
+
+# config sections accessed by this class
 section_names = 'FMU', 'General'
-    
+
+
 class FMU:
+    """
+    this class handels the interaction with the FMU object to seperate it from the gymnasium environment
+    """
+
     def __init__(self, config):
         for name in section_names:
             self.__dict__.update(config.get(name))
-        
-        self.readFMU()
-        self.initFMU()      
 
-    # read FMU
+        self.readFMU()
+        logger.debug('Initializing FMU')
+        self.fmu.instantiate()
+        self.initFMU()
+        logger.debug('Reading IOs FMU')
+        self.getIO()
+
+    # unzip and open FMU
     def readFMU(self):
-        if DEBUG: print('Starting to read FMU')
-        if LOG: print('Using: {}'.format(self.fmuPath))
+        logger.debug('Starting to read FMU')
+        logger.info('Using: {}'.format(self.fmuPath))
         self.description = read_model_description(self.fmuPath)
         self.zipDir = extract(self.fmuPath)
         self.fmu = FMU2Slave(guid=self.description.guid,
                              unzipDirectory=self.zipDir,
                              modelIdentifier=self.description.coSimulation.modelIdentifier,
                              instanceName='instance0')
-        if DEBUG: print('Success creating FMU object')
-        if LOG: print('Unzipped in {}'.format(self.zipDir))
+        logger.debug('Success creating FMU object')
+        logger.info('Unzipped in {}'.format(self.zipDir))
 
     # initialize FMU
     def initFMU(self):
-        if DEBUG: print('Initializing FMU')
-        self.fmu.instantiate()
-        self.fmu.setupExperiment(startTime=self.startTime, tolerance=1e-6, 
-                stopTime=self.stopTime+self.dt)
+        self.fmu.setupExperiment(startTime=self.startTime, tolerance=1e-6,
+                                 stopTime=self.stopTime+self.dt)
+        # set initial inputs
         self.fmu.enterInitializationMode()
         self.fmu.exitInitializationMode()
-        if DEBUG: print('Reading IOs FMU')
-        self.getIO()
 
     # reset FMU to initial state
     def resetFMU(self):
-        if DEBUG: print('Initializing FMU')
+        logger.debug('Initializing FMU')
         self.fmu.reset()
-        self.fmu.setupExperiment(startTime=self.startTime, tolerance=self.tolerance,
-                stopTime=self.stopTime)
-        
-        # set initial inputs
-        self.fmu.enterInitializationMode()
-
-        self.fmu.exitInitializationMode()
+        self.initFMU()
 
     # get IO description of FMU
     def getIO(self):
         self.input = [x for x in self.description.modelVariables
-                if x.name.startswith(tuple(['I_','Control']))]
-        self.output = [x for x in self.description.modelVariables 
-                if x.name.startswith(tuple(['O_','Measurement']))]
-        self.vrs = [x for x in self.description.modelVariables 
-                if not x.name.startswith(tuple(['O_','I_','Control','Measurement']))]
+                      if x.name.startswith(tuple(['I_', 'Control']))]
+        self.output = [x for x in self.description.modelVariables
+                       if x.name.startswith(tuple(['O_', 'Measurement']))]
+        self.vars = [x for x in self.description.modelVariables
+                     if not x.name.startswith(tuple(['O_', 'I_', 'Control', 'Measurement']))]
 
-        #put in same order as GUI
+        # put in same order as GUI
         self.input = self.input[::-1]
         self.output = self.output[::-1]
-        self.vrs = self.vrs[::-1]
+        self.vars = self.vars[::-1]
 
         self.Inames = [x.name for x in self.input]
         self.Onames = [x.name for x in self.output]
 
-        if LOG: 
-            print('\nFound Inputs:')
-            for i, x in enumerate(self.input):
-                print('{}: {}'.format(i,x))
-            print('\nFound Outputs:')
-            for i, x in enumerate(self.output):
-                print('{}: {}'.format(i,x))
-            print('\nFound Vars:')
-            for i, x in enumerate(self.vrs):
-                print('{}: {}'.format(i,x))
+        logger.debug('\nFound Inputs:')
+        for i, x in enumerate(self.input):
+            logger.debug('{}: {}'.format(i, x))
+        logger.debug('\nFound Outputs:')
+        for i, x in enumerate(self.output):
+            logger.debug('{}: {}'.format(i, x))
+        logger.debug('\nFound Vars:')
+        for i, x in enumerate(self.vars):
+            logger.debug('{}: {}'.format(i, x))
 
     # terminate FMU after simulation
     def closeFMU(self):
-        if LOG: print('Close fmu and try deleting unzipdir')
+        logger.info('Close fmu and try deleting unzipdir')
         self.fmu.terminate()
         self.fmu.freeInstance()
         shutil.rmtree(self.zipDir, ignore_errors=True)
-        
 
-    #-------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------------
     # getter/setter-functions
-    #-------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------------
+
     def getInput(self):
         return self.input
 
@@ -102,4 +103,3 @@ class FMU:
 
     def getNumOutput(self):
         return int(len(self.output))
-        
