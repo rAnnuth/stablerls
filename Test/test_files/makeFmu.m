355656ec6ff71b77463e00a36004c796e9266b5f
Robert Annuth <robert.annuth@tuhh.de>
2023-02-10 17:41:15 +0100

    adding filed for the tests

--- Test/test_files/makeFmu.m
+++ Test/test_files/makeFmu.m
@@ -1,21 +1,24 @@
 clear all
 clc
-clearvars
 Simulink.data.dictionary.closeAll('-discard')
 
 Ts = 1e-6; %sampling time of fmu
 system = 'SimulinkModel';
 
-
+% open system and create port structure
 open_system(system)
-BusSystem
+getports(system)
+set_param(system,'DataDictionary','BusSystem.sldd');
+
+% setup the required solver
 set_param(system,'SolverType','Fixed-step')
 set_param(system,'FixedStep',string(Ts))
 
+% let matlab calculate the output bus
 set_param([system '/Measurement'],'OutDataTypeStr','Inherit: auto');
 busInfo = Simulink.Bus.createObject(system,[system '/Measurement']);
 set_param([system '/Measurement'],'OutDataTypeStr',['Bus: ' busInfo.busName]);
 
+% build FMU
 fprintf('Starting Build process ... \n')
-
 exportToFMU2CS(system, 'CreateModelAfterGeneratingFMU', 'off', 'AddIcon', 'off');
