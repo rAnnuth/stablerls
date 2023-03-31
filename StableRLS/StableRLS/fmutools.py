6daf878022a8df949899f4091866beada9802e23
Robert Annuth <robert.annuth@tuhh.de>
2023-03-31 12:28:37 +0200

    fmutools.py to black - All black now

--- StableRLS/StableRLS/fmutools.py
+++ StableRLS/StableRLS/fmutools.py
@@ -8,7 +8,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 # config sections accessed by this class
-section_names = 'FMU', 'General'
+section_names = "FMU", "General"
 
 
 class FMU:
@@ -24,63 +24,62 @@ class FMU:
     """
 
     def __init__(self, config):
-        """Constructor method
-        """
+        """Constructor method"""
         # make parameters of config file available as class parameters
         for name in section_names:
             self.__dict__.update(config.get(name))
 
         # this is the default fmpy procedure
         self.readFMU()
-        logger.debug('Initializing FMU')
+        logger.debug("Initializing FMU")
         self.fmu.instantiate()
         self.initFMU()
-        logger.debug('Reading IOs FMU')
+        logger.debug("Reading IOs FMU")
         self.getIO()
 
-    # 
+    #
     def readFMU(self):
-        """Unzip and open FMU file
-        """
-        logger.debug('Starting to read FMU')
-        logger.info('Using: {}'.format(self.fmuPath))
+        """Unzip and open FMU file"""
+        logger.debug("Starting to read FMU")
+        logger.info("Using: {}".format(self.fmuPath))
         self.description = read_model_description(self.fmuPath)
         self.zipDir = extract(self.fmuPath)
-        self.fmu = FMU2Slave(guid=self.description.guid,
-                             unzipDirectory=self.zipDir,
-                             modelIdentifier=self.description.coSimulation.modelIdentifier,
-                             instanceName='instance0')
-        logger.debug('Success creating FMU object')
-        logger.info('Unzipped in {}'.format(self.zipDir))
+        self.fmu = FMU2Slave(
+            guid=self.description.guid,
+            unzipDirectory=self.zipDir,
+            modelIdentifier=self.description.coSimulation.modelIdentifier,
+            instanceName="instance0",
+        )
+        logger.debug("Success creating FMU object")
+        logger.info("Unzipped in {}".format(self.zipDir))
 
     # initialize FMU
     def initFMU(self):
-        """Fmpy requires to setup an experiment before the simulation can be started
-        """
-        self.fmu.setupExperiment(startTime=self.start_time, tolerance=1e-6,
-                                 stopTime=self.stop_time+self.dt)
+        """Fmpy requires to setup an experiment before the simulation can be started"""
+        self.fmu.setupExperiment(
+            startTime=self.start_time, tolerance=1e-6, stopTime=self.stop_time + self.dt
+        )
         # set initial inputs
         self.fmu.enterInitializationMode()
         self.fmu.exitInitializationMode()
 
     # reset FMU to initial state
     def resetFMU(self):
-        """Reset the FMU to the inital state
-        """
-        logger.debug('Initializing FMU')
+        """Reset the FMU to the inital state"""
+        logger.debug("Initializing FMU")
         self.fmu.reset()
         self.initFMU()
 
     # get IO description of FMU
     def getIO(self):
-        """Read all available inputs and outputs of the FMU
-        """
+        """Read all available inputs and outputs of the FMU"""
         self.output = [x.variable for x in self.description.outputs]
-        self.input = [x for x in self.description.modelVariables
-                        if x not in self.output]
+        self.input = [
+            x for x in self.description.modelVariables if x not in self.output
+        ]
         # remove time variable since we dont want to set it
         for i, x in enumerate(self.input):
-            if x.name == 'time':
+            if x.name == "time":
                 self.input.pop(i)
                 break
 
@@ -91,17 +90,16 @@ class FMU:
         self.input_names = [x.name for x in self.input]
         self.output_names = [x.name for x in self.output]
 
-        logger.info('Found Inputs access with corresponding number:')
+        logger.info("Found Inputs access with corresponding number:")
         for i, x in enumerate(self.input_names):
-            logger.debug('{}: {}'.format(i, x))
-        logger.info('Found Outputs access with corresponding number:')
+            logger.debug("{}: {}".format(i, x))
+        logger.info("Found Outputs access with corresponding number:")
         for i, x in enumerate(self.output_names):
-            logger.debug('{}: {}'.format(i, x))
+            logger.debug("{}: {}".format(i, x))
 
     def closeFMU(self):
-        """Terminate FMU after simulation
-        """
-        logger.info('Close fmu and try deleting unzipdir')
+        """Terminate FMU after simulation"""
+        logger.info("Close fmu and try deleting unzipdir")
         self.fmu.terminate()
         self.fmu.freeInstance()
         shutil.rmtree(self.zipDir, ignore_errors=True)
