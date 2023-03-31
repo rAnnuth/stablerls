7357fa0911f9d844c8bdf2dd60cfca64f039d7e8
Robert Annuth <robert.annuth@tuhh.de>
2023-03-31 12:27:15 +0200

    createFMU.py is now also black

--- StableRLS/StableRLS/createFMU.py
+++ StableRLS/StableRLS/createFMU.py
@@ -4,7 +4,7 @@ import os
 
 # specify section containing FMU path
 # ----------------------------------------------------------------------------
-section_names = 'FMU'
+section_names = "FMU"
 # ----------------------------------------------------------------------------
 
 
@@ -12,11 +12,11 @@ def createFMU(cfg):
     """See https://www.mathworks.com/help/matlab/matlab_external/install-the-matlab-engine-for-python.html
     for the installation of matlab engine. The engine is required to run this function.
 
-    The function searches for a simulink model defined in the config dict and compiles it into an FMU. 
+    The function searches for a simulink model defined in the config dict and compiles it into an FMU.
 
     Parameters:
     ------
-    cfg: dict 
+    cfg: dict
         Dictionary containing the keys 'fmuPath', 'dt' within the section specified above
         (default is 'FMU')
     """
@@ -24,15 +24,15 @@ def createFMU(cfg):
     script_folder = os.path.dirname(os.path.abspath(__file__))
 
     # start matlab engine
-    eng = matlab.engine.start_matlab('-nosplash -noFigureWindows -r')
-    slxDir = os.path.dirname(cfg.get(section_names)['fmuPath'])
-    mdl = os.path.splitext(os.path.basename(cfg.get(section_names)['fmuPath']))[0]
+    eng = matlab.engine.start_matlab("-nosplash -noFigureWindows -r")
+    slxDir = os.path.dirname(cfg.get(section_names)["fmuPath"])
+    mdl = os.path.splitext(os.path.basename(cfg.get(section_names)["fmuPath"]))[0]
 
     # the code is also available as matlab code /Example/matlab/template/makeFMU.m
     eng.eval(f"cd('{slxDir}')", nargout=0)
     eng.eval(f"mdl = '{mdl}';", nargout=0)
     print(script_folder)
-    
+
     eng.eval(f"addpath('{script_folder}')", nargout=0)
 
     # dt specifies the step time of the FMU
@@ -48,6 +48,8 @@ def createFMU(cfg):
 
     # start the fmu creation process and quit
     eng.eval(
-        "exportToFMU2CS(mdl, 'CreateModelAfterGeneratingFMU', 'off', 'AddIcon', 'off');", nargout=0)
+        "exportToFMU2CS(mdl, 'CreateModelAfterGeneratingFMU', 'off', 'AddIcon', 'off');",
+        nargout=0,
+    )
     eng.eval("Simulink.data.dictionary.closeAll('-discard')", nargout=0)
     eng.quit()
