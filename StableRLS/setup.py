e5c2ee689557ddee927209430f08df7c7d574a94
Robert Annuth <robert.annuth@tuhh.de>
2023-07-09 17:07:08 +0200

    removing matlabengine from requirements

--- StableRLS/setup.py
+++ StableRLS/setup.py
@@ -2,19 +2,18 @@ import setuptools
 
 setuptools.setup(
   name='StableRLS',
-  version='0.2.0',
+  version='1.0.0',
   author='Robert Annuth',
   author_email='robert.annuth@tuhh.de',
   packages= setuptools.find_packages(),
   license='LICENSE.txt',
   description='Simulate Simulink FMUs as Gymnasium environment',
   url='https://github.com/rAnnuth/stablerls',
-  keywords='simulation RL reinforcement learning simulink matlab FMU',
+  keywords='simulation RL reinforcement learning Simulink matlab FMU',
   install_requires=[
       "numpy",
       "fmpy",
       "gymnasium",
       "pytest-ordering",
-      "matlabengine",
   ],
 )
